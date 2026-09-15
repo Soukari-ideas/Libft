6b55891e8ad6e9dad83a05a245f4aa618297e92b
mr-coding <mathys.pro.sen@gmail.com>
2026-09-15 22:51:15 +0200

    correct memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,27 +1,45 @@
 #include <stddef.h>
 
-void	*memmove(void *dest, const void *src, size_t n)
-{	
-	if (n == 0)
-		return (dest);
-	size_t i;
-	const unsigned char	*ptr;
-	unsigned char	*ptr2;
-	unsigned char tab[n];
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t			i;
 
-	i = 0;
-	ptr = (const unsigned char *)src;
-	while (i < n)
+	if (n == 0 || dest == src)
+		return (dest); // on coupe le programme si il n'y a rien a copier ou si dest et src start au mm endroit
+	d = dest; // pointe sur le premier octet de destination
+	s = src; //  pointe sur le premier octet de source
+	i = 0; //    etablis un simple compteur
+	while (i < n && d != s + i) 
+		i++; 
+	if (i < n)
 	{
-		tab[i] = ptr[i];
-		i++;
+		while (n > 0)
+		{
+			n--;
+			d[n] = s[n]; // d[5 -1] = d[4 + 5 -1] d4 = t puis un s puis un e et enfin un espace.
+		}
 	}
-	i = 0;
-	ptr2 = (unsigned char *)dest;
-	while (i < n)
+	else
 	{
-		ptr2[i] = tab[i];
-		i++;
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i]; 
+			i++;
+		}
 	}
 	return (dest);
-}	
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	test_source[20] = "Ceci est un test.";
+	ft_memmove(test_source, test_source + 5, 6);
+	printf("%s\n", test_source);
+	return (0);
+}
+*/
